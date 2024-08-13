aa0e3548d5cf4c4aa5884114f8312f7bd7391771
Danrley Linhares <danrleylinhares@gmail.com>
2024-08-13 18:44:03 -0300

    feat: bcrypt and nodemoon

--- src/service/user.services.js
+++ src/service/user.services.js
@@ -1,9 +1,13 @@
 import userRepositories from "../repositories/user.repositories.js";
+import bcrypt from 'bcrypt';
 
 async function createUserService(newUser) {
     const foundUser = await userRepositories.findUserByEmailRepository(newUser.email)
     if (foundUser) throw new Error("User already exists!")
-    const user = await userRepositories.createUserRepository(newUser)
+
+    const passHash = await bcrypt.hash(newUser.password, 10);
+    const user = await userRepositories.createUserRepository({...newUser, password: passHash});
+    if (!user) throw new Error("Error creating User");
     return user;
 }
 
